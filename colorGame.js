cb7855da8da377dc1a4fb8aa841bf12cbc323d36
samanthabertous <sam@samanthabertous.com>
2016-06-10 17:52:53 -0400

    initialize game

---
+++ colorGame.js
@@ -0,0 +1,47 @@
+var colors = [
+	"rgb(255, 0, 0)",
+	"rgb(255, 255, 0)",
+	"rgb(0, 255, 0)",
+	"rgb(0, 255, 255)",
+	"rgb(0, 0, 255)",
+	"rgb(255, 0, 255)"
+]
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var colorDisplay = document.querySelector("#colorDisplay");
+var mesageDisplay = document.querySelector("#message")
+colorDisplay.textContent = pickedColor;
+
+for (var i = 0; i < squares.length; i++) {
+	//add initial colors to squares
+	squares[i].style.background = colors[i];
+
+	// add click listeners to squares
+	squares[i].addEventListener("click", function(){
+		//grab color of clicked square
+		var clickedColor = this.style.background
+		//compare colot to pickedColor
+		if(clickedColor === pickedColor){
+			mesageDisplay.textContent = "Correct";
+			changeColors(clickedColor);
+		} else {
+			this.style.background = "#232323";
+			mesageDisplay.textContent = "Try Again";
+
+		}
+
+	})
+}
+
+function changeColors(colors){
+	//change all sqaures to match given color
+	for (var i = 0; i < squares.length; i++) {
+		squares[i].style.background = colors;
+	};
+}
+
+function pickColor(){
+	 var random = Math.floor(Math.random() * colors.length);
+	 return colors[random];
+}
